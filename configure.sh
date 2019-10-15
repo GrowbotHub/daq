92b76acd14008187e885d4309801303dea657dfb
Darko Lukic <lukicdarkoo@gmail.com>
2019-10-15 14:10:41 +0200

    Initial commit

---
+++ configure.sh
@@ -0,0 +1,19 @@
+SERVICE_NAME=growbothub-daq.service
+SERVICE_PATH=/etc/systemd/system/${SERVICE_NAME}
+
+# Populate service file
+sudo bash -c "cat > ${SERVICE_PATH}" << EOF
+[Unit]
+Description=GrowbotHub DAQ
+
+[Service]
+ExecStart=/usr/bin/python3 $(pwd)/daq.py
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo chmod 664 ${SERVICE_PATH}
+sudo systemctl daemon-reload
+sudo systemctl enable ${SERVICE_NAME}
+sudo systemctl start ${SERVICE_NAME}
---
+++ daq.py
@@ -0,0 +1,37 @@
+from picamera import PiCamera
+from time import sleep
+from datetime import datetime
+import os
+
+DIST = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'images')
+PERIOD_HOURS = 0.01
+
+
+def get_filepath():
+	now = datetime.now()
+	timestamp = now.strftime('%y-%m-%d_%H-%M-%S')
+	img_name = 'image_{}.jpg'.format(timestamp)
+	return os.path.join(DIST, img_name)
+	
+
+def take_picture(camera):
+	filepath = get_filepath()
+	camera.capture(filepath)
+	print('New image saved to `{}`'.format(filepath))
+
+
+def main():
+	if not os.path.exists(DIST):
+		os.makedirs(DIST)
+
+	print('Initializing camera...')
+	camera = PiCamera()
+	print('Camera initialized')
+	while True:
+		take_picture(camera)
+		sleep(60 * 60 * PERIOD_HOURS)
+
+
+if __name__ == '__main__':
+	main()
+
